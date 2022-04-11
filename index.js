be01ce7ce69d3cf6c3e77064704bf2986fbc0a5f
wl01651473@gmail.com <wL01651473@gmail.com>
2022-04-11 23:23:14 +0800

    test1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function beginSliding(e) {
     slider.onpointermove = slide;
     slider.setPointerCapture(e.pointerId);
+    slider2.onpointerenter = beingOver;
+    console.log(e.pointerId);
 }
 
 function stopSliding(e) {
@@ -9,10 +11,16 @@ function stopSliding(e) {
 }
 
 function slide(e) {
-    slider.style.transform = `translate(${e.clientX - 70}px)`;
+    // console.log(e);
+    slider.style.transform = `translate(${e.clientX - 70}px,${e.clientY - slider.offsetHeight / 2}px)`;
+}
+
+function beingOver(e) {
+    console.log(e);
 }
 
 const slider = document.getElementById("slider");
-
+const slider2 = document.getElementById("slider2");
+slider2.onpointerenter = beingOver;
 slider.onpointerdown = beginSliding;
 slider.onpointerup = stopSliding;
